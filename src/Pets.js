38747a5315e857d23c6158a2b898423fee1795eb
Sanyam Gupta <sanyamg37@gmail.com>
2023-03-22 18:28:21 -0700

    :(

--- src/Pets.js
+++ src/Pets.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import petData from './petData';
+import petData from "./petData";
 
 class Pets extends Component {
   constructor(props) {
@@ -8,7 +8,7 @@ class Pets extends Component {
       petType: this.props.petType,
       value: "Dog",
       chosen: false,
-      display: false
+      display: false,
     };
   }
 
@@ -16,61 +16,70 @@ class Pets extends Component {
     this.setState({ value: event.target.value });
   }
 
-  selectOptions(){
-  var select = document.getElementById("petType");
-  let ani = this.state.petType;
-  let arra = [];
-  let same = false;
-  let count = 0;
-  for (let i = 0; i < ani.length; i++) {
-    for (let j = 0; j < arra.length; j++) {
-          if ( ani[i].type == arra[j] ) {
-               same = true;
-          }
-    }
-    count++;
-  if (count == 1 && same == false) {
-          arra.push(ani[i].type);
-  }
+  selectOptions() {
+    var select = document.getElementById("petType");
+    let ani = this.state.petType;
+    let arra = [];
+    let same = false;
+    let count = 0;
+    for (let i = 0; i < ani.length; i++) {
+      for (let j = 0; j < arra.length; j++) {
+        if (ani[i].type == arra[j]) {
+          same = true;
+        }
+      }
+      count++;
+      if (count == 1 && same == false) {
+        arra.push(ani[i].type);
+      }
       same = false;
       count = 0;
-  }
-  arra.map((type) => {
-    return (
-        <option value={type}> {type} </option>
-    )
-  })
-  console.log(arra); //code above is trying to find all unique types in array of petType
+    }
+    arra.map((type) => {
+      document.getElementById("petType").innerHTML = "<option value=" + { type } + ">" + { type } + "</option>";
+    });
+    console.log(arra); //code above is trying to find all unique types in array of petType
   }
 
-  handleDisplay = () =>{ //meant to create a table that displays to page
+  handleDisplay = () => {
+    //meant to create a table that displays to page
     let cute = this.state.petType;
-    console.log("test");
-
-    const imgStyle = {display: 'block', height: '50%', width: '50%', flex: 1};
-      
-    for(let a = 0; a < cute.length; a++ ){
-      if(this.state.value == cute[a].type){
-        return(
+    const imgStyle = { display: "block", height: "50%", width: "50%", flex: 1 };
+    for (let anNum = 0; anNum < cute.length; anNum++) {
+      if (this.state.value == cute[anNum].type) {
+        return (
           <table>
+            {" "}
             <tr>
+              {" "}
               <th>
-                <h1>Their name is {cute[a].name}</h1>
+                {" "}
+                <h1>Their name is + {cute[anNum].name}+ '</h1>{" "}
               </th>
             </tr>
             <tr>
               <td>
-                <img style = {imgStyle} id = {cute[a]._id} name = {cute[a].name} alt = {cute[a].breed} src = {cute[a].url} >
-                </img>
+                {" "}
+                <img
+                  style={imgStyle}
+                  id={cute[anNum]._id}
+                  name={cute[anNum].name}
+                  alt={cute[anNum].breed}
+                  src={cute[anNum].url}
+                ></img>
               </td>
               <td>
-                <h1>They cost $ {cute[a].price}</h1>
+                <h1>They cost $ {cute[anNum].price}</h1>
               </td>
+            </tr>
+            <tr>
+              <button value={cute[anNum]}>
+                Click here to learn more about them
+              </button>
             </tr>
           </table>
         );
       }
-      console.log("as");
     }
   };
 
@@ -91,14 +100,14 @@ class Pets extends Component {
   // }
 
   render() {
-    if(!this.state.chosen){
-      if(this.state.display){
-        return(
+    if (!this.state.chosen) {
+      if (this.state.display) {
+        return (
           <div>
             {this.handleDisplay()}
             <div className="Pets">
               {this.selectOptions()}
-              <form onSubmit={this.state.display = true}>
+              <form onSubmit={(this.state.display = true)}>
                 <h2>What animals would you like to look at?</h2>
                 <select
                   name="animals"
@@ -115,13 +124,12 @@ class Pets extends Component {
               </form>
             </div>
           </div>
-          
         );
       }
       return (
         <div className="Pets">
           {this.selectOptions()}
-          <form onSubmit={this.state.display = true}>
+          <form onSubmit={(this.state.display = true)}>
             <h2>What animals would you like to look at?</h2>
             <select
               name="animals"
@@ -138,11 +146,8 @@ class Pets extends Component {
           </form>
         </div>
       );
-    }
-    else{
-      return(
-        <petData/>
-      )
+    } else {
+      return <petData />;
     }
   }
 }
