df0c6d653ff1fc4c0fb74b49c8f35288121fa5e8
TrevorDrayton03 <trevdray@hotmail.com>
2023-08-09 20:40:17 -0700

    remove border on hover and select for the brand logo

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -41,7 +41,7 @@ class NavigationBar extends Component {
     render() {
         return (
             < nav className="navbar navbar-expand-lg large navbar-custom" >
-                <a className={"navbar-brand " + (this.props.route === "/" ? "navbar-brand-active" : "")} href="#" onClick={e => this.handleOnClick(e, "/")}>
+                <a className={"navbar-brand "} href="#" onClick={e => this.handleOnClick(e, "/")}>
                     <div className="col">
                         <img src="./logo192.png" alt="Logo" />
                         <span className="logo-text">Pet Universe</span>
